3a7eabbfb8b94a339dd5e80b30df1f5ed6a4bbee
mengweijin <1002284406@qq.com>
2022-05-14 19:07:35 +0800

    page

--- src/main/java/com/github/mengweijin/quietly/system/controller/CaseDefinitionController.java
+++ src/main/java/com/github/mengweijin/quietly/system/controller/CaseDefinitionController.java
@@ -1,5 +1,7 @@
 package com.github.mengweijin.quietly.system.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.PageDTO;
 import com.github.mengweijin.quietly.system.entity.CaseDefinition;
 import com.github.mengweijin.quietly.system.service.CaseDefinitionService;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -38,6 +41,11 @@ public class CaseDefinitionController  {
      */
     @Autowired
     private CaseDefinitionService caseDefinitionService;
+
+    @GetMapping("/page")
+    public PageDTO<CaseDefinition> page(PageDTO<CaseDefinition> page, @Valid @RequestParam CaseDefinition caseDefinition) {
+        return caseDefinitionService.page(page, new QueryWrapper<>(caseDefinition));
+    }
 
     @GetMapping("/runCase/{id}")
     public void runCase(@PathVariable("id") Long id) {
