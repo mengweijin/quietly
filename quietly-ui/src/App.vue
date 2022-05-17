8b0cee54fb7f0f8206bfd3dcda7b2b1c2e1a6cea
mengweijin <1002284406@qq.com>
2022-05-17 20:17:40 +0800

    locale

--- quietly-ui/src/App.vue
+++ quietly-ui/src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <router-view />
+  <el-config-provider :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
 
 <script setup>
 
+</script>
+
+<!-- /** el-config-provider: Element-Plus global default language setting */ -->
+<script>
+  import { defineComponent } from 'vue'
+  import { ElConfigProvider } from 'element-plus'
+  import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+  export default defineComponent({
+    components: {
+      ElConfigProvider,
+    },
+    setup() {
+      return {
+        locale: zhCn,
+      }
+    },
+  })
 </script>
 
 <style>
